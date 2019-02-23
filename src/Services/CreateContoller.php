1fb3214264e731e839462569f007f4aeb914bd14
Dylan <dylan.baine@yahoo.com>
2019-02-22 23:02:01 -0600

    add constructor validation to generate crud controller

--- src/Services/CreateContoller.php
+++ src/Services/CreateContoller.php
@@ -2,34 +2,60 @@
 namespace Baine\AutoCrud\Services;
 
 use File;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateController
 {
-  public function __construct(string $model)
+  public function __construct(string $model, bool $addRoutes = false)
   {
+    $this->verifyModel($model);
     $this->model = $model;
+    $this->explodedModelNamespace = explode('\\', $this->model);
+  }
+  private function verifyModel($model)
+  {
+    if (!new $model instanceof Model) {
+      throw new \InvalidArgumentException("[$model] must be an instance of " . Model::class . " to generate a CRUD Controller.");
+    }
   }
   public function execute()
   {
-    $explodedModel = explode('\\', $this->model);
-    $modelClassName = array_last($explodedModel);
+    $stub = $this->fillStub([
+      'model' => $this->getModelClassName(),
+      'fillArray' => $this->generateFillArrayString(),
+      'fullModelNamespace' => $this->model . ';',
+      'controllerNamespace' => $this->getControllerNamespace()
+    ]);
+    $controllerName = $this->getControllerName();
+    File::put(app_path("Http/Controllers/$controllerName"), $stub);
+  }
+
+  protected function getModelClassName()
+  {
+    return array_last($this->explodedModelNamespace);
+  }
+
+  protected function getControllerName()
+  {
+    return $this->getModelClassName() . "Controller.php";
+  }
+
+  protected function getControllerNamespace()
+  {
+    return 'App\\Http\\Controllers';
+  }
+
+  protected function generateFillArrayString()
+  {
     $fillableArray = (new $this->model)->getFillable();
-    $fillArray = [];
-    foreach ($fillableArray as $value) {
-      $fillArray[$value] = '$request->' . "$value";
-    }
     $tabs = "           ";
     $fillArrayString = "[\n";
-    foreach ($fillArray as $key => $value) {
-      $fillArrayString .= "$tabs'$key' => $value,\n";
+    $fillArray = [];
+    foreach ($fillableArray as $key => $value) {
+      $fillArrayString .= "$tabs'$value' => \$request->$value,\n";
     }
     $fillArrayString .= "        ]";
-    $stub = $this->fillStub([
-      'model' => $modelClassName,
-      'fillArray' => $fillArrayString,
-      'fullModelNamespace' => $this->model . ';',
-    ]);
-    File::put(app_path('Http/Controllers/' . $modelClassName . 'Controller.php'), $stub);
+    return $fillArrayString;
   }
 
   protected function fillStub(array $keyVals)
